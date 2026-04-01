8b48dd7718ff8701cc0666276beb0a6d5c2166e8
Carl-rai <rai.carlchristian@gmail.com>
2026-04-01 10:54:07 +0800

    kaupat

--- backend/settings.py
+++ backend/settings.py
@@ -199,6 +199,7 @@ EMAIL_USE_TLS = get_bool_env('EMAIL_USE_TLS', True)
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+# FRONTEND_URL = config('FRONTEND_URL')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
