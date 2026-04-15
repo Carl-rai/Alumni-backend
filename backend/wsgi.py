1154148961879bcc2e8d59084ec1dc4a22b70e60
Carl-rai <rai.carlchristian@gmail.com>
2026-04-15 13:21:06 +0800

    kawalo revice

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+import django
+from django.apps import apps
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+if not apps.ready:
+    django.setup()
+
+if os.getenv("RUN_MIGRATIONS_ON_STARTUP", "true").strip().lower() in {"1", "true", "yes", "on"}:
+    call_command("migrate", interactive=False, verbosity=0)
 
 application = get_wsgi_application()
